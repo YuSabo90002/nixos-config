2c6832d06dd078c528464b8c024f95204e165cff
yuta <yusabo90002@gmail.com>
2026-05-02 14:04:33 +0900

    カーソルテーマに Bibata-Modern-Classic を設定

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -81,6 +81,15 @@ in {
     };
   };
 
+  home.pointerCursor = {
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic";
+    size = 24;
+    gtk.enable = true;
+    x11.enable = true;
+    hyprcursor.enable = true;
+  };
+
   # ランチャーはAGSに統合済み（ags/widget/Launcher.tsx）
 
   programs.git = {
