a973b037892ceed1c96953b2b4a71345dc21b0a3
yuta <yusabo90002@gmail.com>
2026-08-09 01:32:08 +0900

    Revert "IPv6 抑制を SSID 単位でも指定できるようにする"
    This reverts commit 28809639768c52cea42113dca9543b4f3b80c28a.

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -1,19 +1,15 @@
 { config, lib, ... }:
 let
-  # RA を蹴って IPv4 のみにする設定。
-  suppressed = {
-    DHCP = "ipv4";
-    IPv6AcceptRA = false;
-    LinkLocalAddressing = "ipv4";
-  };
-  normal = {
-    DHCP = "yes";
-  };
-
-  # ホスト全体で抑制するか (据え置き機向け)
-  v6 = if config.my.suppressIPv6 then suppressed else normal;
-
-  suppressSSIDs = config.my.suppressIPv6OnSSIDs;
+  # IPv6 を抑制するホストでは RA を蹴って IPv4 のみにする。
+  # 抑制しないホストは DHCP も RA もそのまま通す。
+  v6 =
+    if config.my.suppressIPv6 then {
+      DHCP = "ipv4";
+      IPv6AcceptRA = false;
+      LinkLocalAddressing = "ipv4";
+    } else {
+      DHCP = "yes";
+    };
 in
 {
   options.my.suppressIPv6 = lib.mkOption {
@@ -27,22 +23,7 @@ in
       して回避している。
 
       持ち出す機体で true にすると、外出先の正常な IPv6 網でも v6 が一切使えなく
-      なるため false のままにすること。代わりに my.suppressIPv6OnSSIDs を使う。
-    '';
-  };
-
-  options.my.suppressIPv6OnSSIDs = lib.mkOption {
-    type = lib.types.listOf lib.types.str;
-    default = [ ];
-    example = [ "Buffalo-17B8" ];
-    description = ''
-      この SSID に繋いでいるときだけ IPv6 を抑制する。持ち出す機体で、自宅など
-      特定の回線でだけ IPv6 が壊れている場合に使う (my.suppressIPv6 の
-      全ホスト版に対する、ネットワーク単位版)。
-
-      systemd-networkd の [Match] SSID= で実現しており、生成する .network の
-      ファイル名を汎用ルールより手前 (24- < 25-) にして優先させる。
-      SSID は空白区切りで並べる仕様のため、空白を含む SSID は指定できない。
+      なるため false のままにすること。
     '';
   };
 
@@ -55,18 +36,6 @@ in
       networkConfig = v6;
       dhcpV4Config.RouteMetric = 100;
     };
-
-    # networkd は最初にマッチした .network だけを適用し、ファイル名の辞書順で
-    # 探すので、SSID 限定のこれを汎用の 25- より手前に置く。
-    systemd.network.networks."24-wireless-suppress-ipv6" =
-      lib.mkIf (suppressSSIDs != [ ]) {
-        matchConfig = {
-          Type = "wlan";
-          SSID = lib.concatStringsSep " " suppressSSIDs;
-        };
-        networkConfig = suppressed;
-        dhcpV4Config.RouteMetric = 600;
-      };
 
     systemd.network.networks."25-wireless" = {
       matchConfig.Type = "wlan";
